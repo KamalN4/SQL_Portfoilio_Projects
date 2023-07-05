ed9bc59ac62cc819f2a274b49c4ebbf6fb7e6d3c
KamalN4 <123488916+KamalN4@users.noreply.github.com>
2023-07-05 16:26:18 +0530

    Update Output_project.sql

--- Output_project.sql
+++ Output_project.sql
@@ -8,9 +8,13 @@ Select count(DISTINCT Hotel_id)
 From Oyo_project.dbo.Hotel_details;
 
 -- No of hotels citywise
-Select city , count(distinct Hotel_id)
+Select city , count(distinct Hotel_id) no_of_hotels
 From Oyo_project.dbo.Hotel_details
 Group By city;
+
+--Avg No of hotels 
+Select count(distinct Hotel_id)/count(distinct city)
+From Oyo_project.dbo.Hotel_details
 
 --Cancellation rate citywise
 Select h2.city , count(status) as cancelled_citywise
@@ -19,19 +23,19 @@ where h1.status = 'Cancelled'
 group by city
 order by city
 
---no of bookings in each city in the month of january , febuary, and march
+--no of bookings in each city 
 Select h2.City , count(h1.date_of_booking) as no_of_bookings
 From Oyo_project.dbo.Booking_details as h1 LEFT JOIN Oyo_project.dbo.Hotel_details as h2 on h1.hotel_id = h2.Hotel_id
-Where h1.Bookingmonth IN (1,2,3)
 Group By City
 
 
 --No of nights stayed by the customers
---using age function
-select AGE(CHECK_OUT,CHECK_IN) AS NO_OF_NIGHTS , count(customer_id) as no_of_customers_stayed  
+--using DATEDIFF function
+select DATEDIFF( DAY,check_in , check_out) no_of_nights_stayed , count(customer_id) no_of_customers
 from Oyo_project.dbo.Booking_details
-group by NO_OF_NIGHTS
-ORDER BY no_of_customers_stayed DESC
+Group by no_of_nights_stayed
+
+
 
 --Customers booking rooms on same day
 Select date_of_booking , count(customer_id) as no_of_customers
@@ -46,11 +50,11 @@ From Oyo_project.dbo.Booking_details
 where check_in> date_of_booking
 
 --Different types of pre-bookings made on basis of day
-SELECT AGE(check_in,date_of_booking) AS DIFFERENCE_IN_DAYS , COUNT(*) AS NO_OF_CUSTOMER,
+SELECT DATEDIFF(DAY , date_of_booking , check_in) AS DIFFERENCE_IN_DAYS , COUNT(*) AS NO_OF_CUSTOMER
 FROM Oyo_project.dbo.Booking_details
-WHERE AGE(check_in,date_of_booking) <> '00:00:00'
+WHERE DIFFERENCE_IN_DAYS <>0
 GROUP BY DIFFERENCE_IN_DAYS
-ORDER BY NO_OF_CUSTOMER DESC
+
 
 --Returning top 10 records as per amount
 SELECT TOP 15 * 
